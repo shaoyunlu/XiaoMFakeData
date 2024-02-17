846380448b53ea19ab249c5376dbabb6e9da4e70
shaoyunlu <shaoyunlu@126.com>
2024-02-17 17:26:53 +0800

    1

--- account/admin.js
+++ account/admin.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const users = Array.from({length : 63} ,()=>({
+    id: 1,
+    name:'hello world',
+    age:90,
+    gender:1,
+    status:1
+}))
+
+function paginate(array ,page_size ,page_number){
+    page_size = parseInt(page_size)
+    page_number = parseInt(page_number)
+    const start = (page_number -1) * page_size
+    return array.slice(start,start + page_size)
+}
+
 // 定义商品相关的路由
 router.get('/list', function(req, res) {
+    let resList = paginate(users,req.query.pageSize,req.query.pageNum)
     setTimeout(()=>{
         res.json({
             success: true,
             payLoad: {
-                  list : [
-                      {name : '菜单三' ,age:"download",gender : '3'},
-                      {name : '菜单四' ,age:"edit",gender : '4'},
-                      {name : '菜单五' ,age:"lineChart",gender : '5'},
-                      {name : '菜单六' ,age:"document",gender : '6'}
-                  ],
-                  total : 4
+                  list : resList,
+                  total : users.length
             }
           });
     },1000)
