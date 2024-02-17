283858c1e5d5ec9a0476a90ab99046607b555633
shaoyunlu <shaoyunlu@126.com>
2024-02-17 13:52:06 +0800

    1

---
+++ account/admin.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+// 定义商品相关的路由
+router.get('/list', function(req, res) {
+    setTimeout(()=>{
+        res.json({
+            success: true,
+            payLoad: {
+                  list : [
+                      {name : '菜单三' ,age:"download",gender : '3'},
+                      {name : '菜单四' ,age:"edit",gender : '4'},
+                      {name : '菜单五' ,age:"lineChart",gender : '5'},
+                      {name : '菜单六' ,age:"document",gender : '6'}
+                  ],
+                  total : 150
+            }
+          });
+    },1000)
+});
+
+// 管理员新增
+router.post('/add' ,(req,res)=>{
+    const jsonData = req.body;
+    // 在这里处理jsonData...
+    console.log(jsonData)
+    setTimeout(()=>{
+        res.json({
+            success : true,
+            payLoad:{}
+        })
+    },1000)
+})
+
+
+// 导出路由
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 4000;
+
+app.use(express.json()); // 使得我们可以解析JSON请求体
+app.use(cors());
+
+var adminRouter = require('./account/admin');
+
+app.use('/web/account/admin', adminRouter);
+
+// 示例路由，返回假数据
+app.get('/web/menu/list', (req, res) => {
+  res.json({
+    success: true,
+    payLoad: [
+                {name : '账号设置' ,value : '1' ,icon:"upload" ,children : [
+                                      {name : '管理员' ,value:'/account/admin'},
+                                      {name : '用户' ,value:'/account/user'}
+                                  ]
+                },
+                {name : '菜单二' ,value : '2' ,icon:"barChart",children : [
+                        {name : '菜单2-1' ,value:'2-1'},
+                        {name : '菜单2-2' ,value:'2-2' ,children : [
+                                {name : '菜单2-2-1' ,value : '2-2-1'},
+                                {name : '菜单2-2-2' ,value : '2-2-2'}
+                            ]}
+                    ]
+                },
+                {name : '菜单三' ,icon:"download",value : '3'},
+                {name : '菜单四' ,icon:"edit",value : '4'},
+                {name : '菜单五' ,icon:"lineChart",value : '5'},
+                {name : '菜单六' ,icon:"document",value : '6'}
+    ]
+  });
+});
+
+app.listen(port, () => {
+  console.log(`服务已启动，监听端口 ${port}`);
+});
